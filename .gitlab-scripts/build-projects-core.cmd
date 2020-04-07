eb944efc253c905126ce9615942c0f93509a4f4d
Vyacheslav Voronenko <git@voronenko.info>
2020-04-07 16:30:10 +0200

    deps-build-tests - core

--- .gitlab-scripts/build-projects-core.cmd
+++ .gitlab-scripts/build-projects-core.cmd
@@ -6,5 +6,3 @@ dotnet build src/Datadog.Trace.ClrProfiler.Managed.Loader/Datadog.Trace.ClrProfi
 dotnet build src/Datadog.Trace.ClrProfiler.Managed/Datadog.Trace.ClrProfiler.Managed.csproj --configuration %CONFIGURATION%
 dotnet build src/Datadog.Trace.OpenTracing/Datadog.Trace.OpenTracing.csproj --configuration %CONFIGURATION%
 dotnet build src/Datadog.Trace/Datadog.Trace.csproj --configuration %CONFIGURATION%
-
-c
