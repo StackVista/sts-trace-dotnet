183f2ce6260b9239dbe643b8a7a563ece90a1817
Vyacheslav Voronenko <git@voronenko.info>
2020-04-07 16:38:34 +0200

    deps-build-tests - core

--- .gitlab-scripts/test-projects-core.cmd
+++ .gitlab-scripts/test-projects-core.cmd
@@ -1,4 +1,11 @@
 rem ls test/**/*.Tests.csproj
+echo dotnet test test/Datadog.Trace.ClrProfiler.Managed.Tests/Datadog.Trace.ClrProfiler.Managed.Tests.csproj --configuration %CONFIGURATION%
 dotnet test test/Datadog.Trace.ClrProfiler.Managed.Tests/Datadog.Trace.ClrProfiler.Managed.Tests.csproj --configuration %CONFIGURATION%
+
+echo "2============="
+echo dotnet test test/Datadog.Trace.Tests/Datadog.Trace.Tests.csproj --configuration %CONFIGURATION%
 dotnet test test/Datadog.Trace.Tests/Datadog.Trace.Tests.csproj --configuration %CONFIGURATION%
+
+echo "3============="
+echo dotnet test test/Datadog.Trace.OpenTracing.Tests/Datadog.Trace.OpenTracing.Tests.csproj --configuration %CONFIGURATION%
 dotnet test test/Datadog.Trace.OpenTracing.Tests/Datadog.Trace.OpenTracing.Tests.csproj --configuration %CONFIGURATION%
