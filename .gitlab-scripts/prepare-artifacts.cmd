5fb529a3ab664ce52e89e3ed239ab8d0a6acd752
Vyacheslav Voronenko <git@voronenko.info>
2020-04-09 17:28:53 +0200

    chore(publish artifacts)

--- .gitlab-scripts/prepare-artifacts.cmd
+++ .gitlab-scripts/prepare-artifacts.cmd
@@ -4,8 +4,7 @@ mkdir OUTCOME\\WindowsInstaller\X86
 mkdir OUTCOME\\Nupkg\
 mkdir OUTCOME\\OpenNupkg\
 
-xcopy deploy\Datadog.Trace.ClrProfiler.WindowsInstaller\bin\Release\x64\ OUTCOME\\WindowsInstaller\X64 /s /e
-xcopy deploy\Datadog.Trace.ClrProfiler.WindowsInstaller\bin\Release\x86\ OUTCOME\\WindowsInstaller\X86 /s /e
-xcopy src\Datadog.Trace\bin OUTCOME\\WindowsInstaller\\Nupkg /s /e
-xcopy src\Datadog.Trace.OpenTracing\bin OUTCOME\\WindowsInstaller\\OpenNupkg /s /e
-
+xcopy deploy\Datadog.Trace.ClrProfiler.WindowsInstaller\bin\Release\x64\* OUTCOME\\WindowsInstaller\X64 /s /e
+xcopy deploy\Datadog.Trace.ClrProfiler.WindowsInstaller\bin\Release\x86\* OUTCOME\\WindowsInstaller\X86 /s /e
+xcopy src\Datadog.Trace\bin\* OUTCOME\\WindowsInstaller\\Nupkg /s /e
+xcopy src\Datadog.Trace.OpenTracing\bin\* OUTCOME\\WindowsInstaller\\OpenNupkg /s /e
