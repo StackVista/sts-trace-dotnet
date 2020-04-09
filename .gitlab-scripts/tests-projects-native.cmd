5d222a6980e2628d52cbe2dc03c7f6c797afb335
Vyacheslav Voronenko <git@voronenko.info>
2020-04-09 18:02:22 +0200

    chore(tests) activated tests

--- .gitlab-scripts/tests-projects-native.cmd
+++ .gitlab-scripts/tests-projects-native.cmd
@@ -1,4 +1,15 @@
 Datadog.Trace.ClrProfiler.Native.Tests.exe --gtest_output=xml
 rem  workingDirectory: $(System.DefaultWorkingDirectory)/test/Datadog.Trace.ClrProfiler.Native.Tests/bin/$(buildConfiguration)/$(buildPlatform)
 
+echo "x64 tests"
+pushd %CI_PROJECT_DIR%\test\Datadog.Trace.ClrProfiler.Native.Tests\bin\Release\x64\
+Datadog.Trace.ClrProfiler.Native.Tests.exe --gtest_output=xml
+popd
+
+echo "x86 tests"
+pushd %CI_PROJECT_DIR%\test\Datadog.Trace.ClrProfiler.Native.Tests\bin\Release\x86\
+Datadog.Trace.ClrProfiler.Native.Tests.exe --gtest_output=xml
+popd
+
+
 rem testResultsFiles: test/**/test*.xml
