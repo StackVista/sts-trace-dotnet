f593f3a1884c865f9903c9cad75d8b38b2b046a8
Vyacheslav Voronenko <git@voronenko.info>
2020-04-10 13:14:01 +0200

    splitted into stages

--- .gitlab-scripts/prepare-artifacts.cmd
+++ .gitlab-scripts/prepare-artifacts.cmd
@@ -1,11 +1,12 @@
 IF EXIST OUTCOME RMDIR /S /Q OUTCOME
 mkdir OUTCOME
-mkdir OUTCOME\\WindowsInstaller\X64
-mkdir OUTCOME\\WindowsInstaller\X86
-mkdir OUTCOME\\Nupkg\
-mkdir OUTCOME\\OpenNupkg\
+mkdir OUTCOME\WindowsInstaller\X64
+mkdir OUTCOME\WindowsInstaller\X86
+mkdir OUTCOME\Nupkg\
+mkdir OUTCOME\OpenNupkg\
 
-xcopy deploy\Datadog.Trace.ClrProfiler.WindowsInstaller\bin\Release\x64\* OUTCOME\\WindowsInstaller\X64 /s /e
-xcopy deploy\Datadog.Trace.ClrProfiler.WindowsInstaller\bin\Release\x86\* OUTCOME\\WindowsInstaller\X86 /s /e
-xcopy src\Datadog.Trace\bin\* OUTCOME\\Nupkg /s /e
-xcopy src\Datadog.Trace.OpenTracing\bin\* OUTCOME\\OpenNupkg /s /e
+xcopy deploy\Datadog.Trace.ClrProfiler.WindowsInstaller\bin\Release\x64\* OUTCOME\WindowsInstaller\X64 /s /e
+xcopy deploy\Datadog.Trace.ClrProfiler.WindowsInstaller\bin\Release\x86\* OUTCOME\WindowsInstaller\X86 /s /e
+xcopy src\Datadog.Trace\bin\* OUTCOME\Nupkg /s /e
+xcopy src\Datadog.Trace.OpenTracing\bin\* OUTCOME\OpenNupkg /s /e
+xcopy src\bin\windows-tracer-home.zip OUTCOME\
