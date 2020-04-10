1c4dd803243c41fd6aac018cbaa32e8672971897
Vyacheslav Voronenko <git@voronenko.info>
2020-04-10 12:56:26 +0200

    splitted into stages

--- .gitlab-scripts/prepare-release-artifacts.cmd
+++ .gitlab-scripts/prepare-release-artifacts.cmd
@@ -6,16 +6,16 @@ ren datadog-* stackstate-*
 popd
 
 echo Processing x86 msi in OUTCOME\WindowsInstaller\X64\en-us\
-pushd OUTCOME\\WindowsInstaller\X86\en-us\
+pushd OUTCOME\WindowsInstaller\X86\en-us\
 ren datadog-* stackstate-*
 popd
 
 echo Processing Nupkg
 pushd OUTCOME\Nupkg\Release
-ren datadog-* stackstate.*
+ren datadog.* stackstate.*
 popd
 
 echo Processing OpenNupkg
 pushd OUTCOME\OpenNupkg\Release
-ren datadog-* stackstate.*
+ren datadog.* stackstate.*
 popd
