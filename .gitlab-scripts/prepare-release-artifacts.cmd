1805c7e7406aec1a4b1bd910ff9eae2bd151b456
Vyacheslav Voronenko <git@voronenko.info>
2020-04-10 12:43:20 +0200

    splitted into stages

--- .gitlab-scripts/prepare-release-artifacts.cmd
+++ .gitlab-scripts/prepare-release-artifacts.cmd
@@ -1,17 +1,21 @@
 rem Dirty: address on c# level.
 
+echo Processing x64 msi in OUTCOME\WindowsInstaller\X64\en-us\
 pushd OUTCOME\WindowsInstaller\X64\en-us\
 ren datadog-* stackstate-*
 popd
 
+echo Processing x86 msi in OUTCOME\WindowsInstaller\X64\en-us\
 pushd OUTCOME\\WindowsInstaller\X86\en-us\
 ren datadog-* stackstate-*
 popd
 
+echo Processing Nupkg
 pushd OUTCOME\Nupkg\Release
-ren datadog-* stackstate-*
+ren datadog-* stackstate.*
 popd
 
+echo Processing OpenNupkg
 pushd OUTCOME\OpenNupkg\Release
-ren datadog-* stackstate-*
+ren datadog-* stackstate.*
 popd
