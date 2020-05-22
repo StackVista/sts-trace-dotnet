674da167d0bf182669129d5dc07615204707f799
Gitlab runner <gitlab@runner.some>
2020-05-22 21:53:41 +0200

    unified dual unbranded and branded artifacts

--- .gitlab-scripts/prepare-release-artifacts.cmd
+++ .gitlab-scripts/prepare-release-artifacts.cmd
@@ -2,20 +2,32 @@ rem Dirty: address on c# level.
 
 echo Processing x64 msi in OUTCOME\WindowsInstaller\X64\en-us\
 pushd OUTCOME\WindowsInstaller\X64\en-us\
+IF EXIST "datadog-*" (
 ren datadog-* stackstate-*
+)
 popd
 
 echo Processing x86 msi in OUTCOME\WindowsInstaller\X64\en-us\
 pushd OUTCOME\WindowsInstaller\X86\en-us\
+IF EXIST "datadog-*" (
 ren datadog-* stackstate-*
+)
 popd
 
 echo Processing Nupkg
 pushd OUTCOME\Nupkg\Release
+
+IF EXIST "datadog.*" (
 ren datadog.* stackstate.*
+)
+
 popd
 
 echo Processing OpenNupkg
 pushd OUTCOME\OpenNupkg\Release
+
+IF EXIST "datadog.*" (
 ren datadog.* stackstate.*
+)
+
 popd
